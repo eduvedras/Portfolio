248f2a12c1de38ff5a4939c6399e158dd02f1ce5
eduvedras <edu.vedras@gmail.com>
2023-07-05 17:26:48 +0100

    Pokemon page ready

--- src/app/Pokemon/page.tsx
+++ src/app/Pokemon/page.tsx
@@ -1,8 +1,13 @@
 'use client'
 export default function Pokemon() {
     return (
-        <main className='px-10 md:px-20 lg:px-40 bg-slate-900'>
-            <div className='pt-10 pb-5'>
+        <main className='px-10 md:px-20 lg:px-40 bg-slate-900 w-full overflow-x-hidden'>
+            <div className='pt-5 pb-5 flex justify-end'>
+                <header className='fixed z-40'>
+                    <button onClick={() => location.href = '/'} className='bg-gray-200 hover:bg-teal-800 rounded-md px-4 py-2 text-lg font-semibold text-gray-700 hover:text-teal-400'>Home</button>
+                </header>
+            </div>
+            <div className='pb-5'>
                 <h3 className='text-5xl font-medium'>Pokemon TCG visualization</h3>
             </div>
             <div className='pt-5 pb-5'>
@@ -49,7 +54,7 @@ export default function Pokemon() {
             <div className='pt-5 pb-5'>
                 <h3 className="text-2xl font-medium">Learn more</h3>
                 <p className='text-md leading-7 pt-2 pb-5 text-gray-200'>
-                    If you want to learn more about the project, click <span className='font-extrabold text-gray-200 hover:text-teal-500'>here</span> to download the report
+                    If you want to learn more about the project, click <span onClick={() => location.href = '/VI-25_Report.pdf'} className='font-extrabold text-gray-200 hover:text-teal-500'>here</span> to read the report
                     of the project and <span onClick={() => location.href = 'https://eduvedras.github.io/Pokemon-Vis/'} className='font-extrabold text-gray-200 hover:text-teal-500'>here</span> to try it for yourself. 
                 </p>
             </div>
