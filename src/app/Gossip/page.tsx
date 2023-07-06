a12764916e97558c6ffaa69f6ca498d49cfaea78
eduvedras <edu.vedras@gmail.com>
2023-07-06 16:07:54 +0100

    Gossip page done

--- src/app/Gossip/page.tsx
+++ src/app/Gossip/page.tsx
@@ -46,7 +46,7 @@ export default function Gossip() {
                     a fourth interface designed to be invoked by other servers (in the case where the servers 
                     are replicated, and need to communicate with each other).
                 </p>
-                <div className="pt-3">
+                <div className="pt-4">
                     <h3 className="text-xl font-medium pb-2">2.1. Teacher interface</h3>
                     <ul>
                         <li className="pb-2 flex">
@@ -55,28 +55,118 @@ export default function Gossip() {
                                     openEnrollments
                                 </p>
                             </div>
-                            <p className="text-md pt-1 text-gray-200">&nbsp; - jajah</p>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- receives the class capacity and opens enrollment on this class</p>
                         </li>
-                        <li className="pb-2">
+                        <li className="pb-2 flex">
                             <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
                                 <p className="py-1 px-2 font-mono">
                                     closeEnrollments
                                 </p>
                             </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- closes class enrollment</p>
                         </li>
-                        <li className="pb-2">
+                        <li className="pb-2 flex">
                             <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
                                 <p className="py-1 px-2 font-mono">
                                     list
                                 </p>
                             </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- lists enrollment status, displaying enrolled students list, canceled enrollment list, and total class capacity</p>
                         </li>
-                        <li>
+                        <li className="flex">
                             <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
                                 <p className="py-1 px-2 font-mono">
                                     cancelEnrollment
                                 </p>
                             </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- receives a student identifier, unsubscribing the student and placing him on the class's canceled list</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-4">
+                    <h3 className="text-xl font-medium pb-2">2.2. Student interface</h3>
+                    <ul>
+                        <li className="pb-2 flex">
+                            <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
+                                <p className="py-1 px-2 font-mono">
+                                    list
+                                </p>
+                            </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- lists enrollment status, displaying enrolled students list, canceled enrollment list, and total class capacity</p>
+                        </li>
+                        <li className="pb-2 flex">
+                            <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
+                                <p className="py-1 px-2 font-mono">
+                                    enroll
+                                </p>
+                            </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- receives the student name and identifier, enrolling the student</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-4">
+                    <h3 className="text-xl font-medium pb-2">2.3. Administrator interface</h3>
+                    <ul>
+                        <li className="pb-2 flex">
+                            <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
+                                <p className="py-1 px-2 font-mono">
+                                    activate
+                                </p>
+                            </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- put the server in ACTIVE mode (this is the default behavior), where it responds to all requests</p>
+                        </li>
+                        <li className="pb-2 flex">
+                            <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
+                                <p className="py-1 px-2 font-mono">
+                                    deactivate
+                                </p>
+                            </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- put the server in IDLE mode. In this mode, the server responds with the error "DISABLED" to all requests from teachers and students</p>
+                        </li>
+                        <li className="pb-2 flex">
+                            <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
+                                <p className="py-1 px-2 font-mono">
+                                    dump
+                                </p>
+                            </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- reports the server state</p>
+                        </li>
+                        <li className="pb-2 flex">
+                            <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
+                                <p className="py-1 px-2 font-mono">
+                                    deactivateGossip
+                                </p>
+                            </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- ends the deferred propagation process between replicas</p>
+                        </li>
+                        <li className="pb-2 flex">
+                            <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
+                                <p className="py-1 px-2 font-mono">
+                                    activateGossip
+                                </p>
+                            </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- starts the deferred propagation process between replicas</p>
+                        </li>
+                        <li className="flex">
+                            <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
+                                <p className="py-1 px-2 font-mono">
+                                    gossip
+                                </p>
+                            </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- forces a replica to do a deferred propagation to the other replica(s)</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-4">
+                    <h3 className="text-xl font-medium pb-2">2.4. Interface between servers</h3>
+                    <ul>
+                        <li className="pb-2 flex">
+                            <div className="rounded-lg border-1 bg-slate-800 border-slate-500 w-fit ">
+                                <p className="py-1 px-2 font-mono">
+                                    propagateState
+                                </p>
+                            </div>
+                            <p className="text-md pt-1 text-gray-200">&nbsp; -- a server sends its status to another replica</p>
                         </li>
                     </ul>
                 </div>
@@ -120,11 +210,29 @@ export default function Gossip() {
                 </p>
             </div>
             <div className='pt-5 pb-5'>
+                <h3 className="text-2xl font-medium">4. Solution</h3>
+                <p className='text-md leading-7 pt-2 pb-5 text-gray-200'>
+                    To solve this problem, our solution was to send the time each subscription was 
+                    made to the server that locally keeps a list of all updates. Then, when the gossip 
+                    occurs, the server compares its list of updates with the one it received and if the 
+                    total number of entries exceeds the capacity, it places the entries that are in both 
+                    lists of updates in temporal order up to the capacity, the surpluses are discarded . If two 
+                    entries were perfectly concurrent, give priority to the primary server.
+                    Another situation that can cause inconsistencies happens when the teacher closes enrollments 
+                    in the primary and an enrollment has been made in the secondary before the gossip takes place. 
+                    To solve this problem we added a closeTime variable that is updated with the time the 
+                    closeEnrollments command takes place, then when the primary server receives a gossip it 
+                    compares the time the enrollment was made with this and decides whether to accept it or not. 
+                    The following video shows the solution in action:
+                </p>
+                <video className='relative rounded-lg mx-auto' width="1000" height="800" controls>
+                    <source src="/Distributed_Systems.mp4" type="video/mp4"></source>
+                </video>
+            </div>
+            <div className='pt-5 pb-5'>
                 <h3 className="text-2xl font-medium">5. Learn more</h3>
                 <p className='text-md leading-7 pt-2 pb-5 text-gray-200'>
-                    If you want to learn more about the project, click <span onClick={() => location.href = '/VI-25_Report.pdf'} className='font-extrabold text-gray-200 hover:text-teal-500'>here</span> to read the report
-                    of the project and <span onClick={() => location.href = 'https://eduvedras.github.io/Pokemon-Vis/'} className='font-extrabold text-gray-200 hover:text-teal-500'>here</span> to try it for yourself. You can
-                    also click <span onClick={() => location.href = 'https://github.com/eduvedras/Pokemon-Vis'} className='font-extrabold text-gray-200 hover:text-teal-500'>here</span> to see the source code of the project.
+                    If you want to learn more about the project, click <span onClick={() => location.href = 'https://github.com/eduvedras/Distributed-Systems/'} className='font-extrabold text-gray-200 hover:text-teal-500'>here</span> to see the source code of the project and its report.
                 </p>
             </div>
         </main>
